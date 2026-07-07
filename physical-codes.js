f64bb329b8a3b571c7f2d5eed6725873c9e7648b
hogarquirozramos-lab <hogarquirozramos@gmail.com>
2026-07-06 23:52:50 -0500

    Create physical-codes.js

---
+++ physical-codes.js
@@ -0,0 +1,9 @@
+// Diccionario de códigos físicos (Llaveros/QR)
+// Formato: 'CODIGO_FISICO': 'id_del_vicamon_en_beasts_js'
+const PHYSICAL_CODES = {
+  'IRON-001': 'irondog',
+  'IRON-002': 'irondog',
+  'IRON-TEST': 'irondog'
+};
+
+module.exports = { PHYSICAL_CODES };
